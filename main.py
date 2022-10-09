fe34acdbc2a31ec2e3f7cc8dc6e269a3d95facf5
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-08 20:25:22 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 
 
+from asyncio.windows_events import NULL
+
+
 numbers = [5, 20, 30, 35, 50]
 
 insval = int(input('Enter the insertion value: '))
 # ******************************
 # Make your Code
 # ******************************
-index = -1
+index = len(numbers)
 for i in range(len(numbers)):
     if(insval <= numbers[i]):
         index = i
         break
-if index != -1:
-    numbers.insert(index,insval)
+numbers.insert(index,insval)
 print (numbers)
